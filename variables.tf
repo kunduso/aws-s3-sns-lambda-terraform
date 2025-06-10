4f304ccbca011303a460c5e6cdda6ddab30aca58
sourav kundu <kunduso@amazon.com>
2025-06-09 22:08:25 -0500

    corrected cloudwatch logs encryption

--- variables.tf
+++ variables.tf
@@ -10,3 +10,13 @@ variable "name" {
   type        = string
   default     = "app-13"
 }
+variable "log_group_prefix" {
+  description = "The name of the log group."
+  type        = string
+  default     = "/aws/lambda/"
+}
+variable "log_retention_days" {
+  description = "Number of days to retain Lambda logs"
+  type        = number
+  default     = 365
+}
