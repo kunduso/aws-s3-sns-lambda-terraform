2cdb86e280b605f7ef53a59a85804bf990876922
sourav kundu <kunduso@amazon.com>
2025-11-25 19:41:47 -0600

    add S3 bucket public access block and lifecycle configuration

--- storage.tf
+++ storage.tf
@@ -2,6 +2,8 @@
 resource "aws_s3_bucket" "bucket" {
   bucket        = "${var.name}-bucket"
   force_destroy = true
+  #checkov:skip=CKV_AWS_18: Access logging not required for this demo bucket
+#checkov:skip=CKV_AWS_144: Cross-region replication not required for this demo bucket
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_versioning
@@ -20,6 +22,29 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_encryption
     apply_server_side_encryption_by_default {
       kms_master_key_id = aws_kms_key.s3_key.arn
       sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_public_access_block
+resource "aws_s3_bucket_public_access_block" "bucket_pab" {
+  bucket                  = aws_s3_bucket.bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_lifecycle_configuration
+resource "aws_s3_bucket_lifecycle_configuration" "bucket_lifecycle" {
+  bucket = aws_s3_bucket.bucket.id
+
+  rule {
+    id     = "abort-multipart-uploads"
+    status = "Enabled"
+
+    abort_incomplete_multipart_upload {
+      days_after_initiation = 3
     }
   }
 }
