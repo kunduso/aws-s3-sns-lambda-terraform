ff688769b2737d68eebd1b7595918f0c64d3485d
sourav kundu <kunduso@amazon.com>
2025-11-25 21:08:57 -0600

    fix: update region reference in local variables for CloudWatch log group ARN

--- data.tf
+++ data.tf
@@ -6,6 +6,6 @@ data "aws_caller_identity" "current" {}
 
 locals {
   principal_root_arn       = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-  principal_logs_arn       = "logs.${data.aws_region.current.name}.amazonaws.com"
-  cloudwatch_log_group_arn = "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:${var.log_group_prefix}${var.name}*"
+  principal_logs_arn       = "logs.${data.aws_region.current.id}.amazonaws.com"
+  cloudwatch_log_group_arn = "arn:aws:logs:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:log-group:${var.log_group_prefix}${var.name}*"
 }
